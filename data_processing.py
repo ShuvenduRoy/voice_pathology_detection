436b86f4669676d2f7a01bac6de0755318d69cd6
Shuvendu Bikash <bikash.shuvendu@gmail.com>
2018-09-10 03:28:39 +0600

    data processing

---
+++ data_processing.py
@@ -0,0 +1,48 @@
+import librosa
+from tqdm import tqdm
+import numpy as np
+import os
+from keras.utils import to_categorical
+
+DATA_PATH = 'data'
+
+
+def get_labels(path=DATA_PATH):
+    labels = os.listdir(path)
+    label_indices = np.arange(0, len(labels))
+    return labels, label_indices, to_categorical(label_indices)
+
+
+def wav2mfcc(file_path, max_len=11):
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    wave = wave[::3]
+    mfcc = librosa.feature.mfcc(wave, sr=16000)
+
+    # If maximum length exceeds mfcc lengths then pad the remaining ones
+    if max_len > mfcc.shape[1]:
+        pad_width = max_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
+
+    # Else cutoff the remaining parts
+    else:
+        mfcc = mfcc[:, :max_len]
+
+    return mfcc
+
+
+def save_data_to_array(path=DATA_PATH, max_len=11):
+    labels, _, _ = get_labels(path)
+
+    for label in labels:
+        # Init mfcc vectors
+        mfcc_vectors = []
+
+        wavfiles = [path +'\\'+ label + '\\' + wavfile for wavfile in os.listdir(path + '\\' + label)]
+        for wavfile in tqdm(wavfiles, "Saving vectors of label - '{}'".format(label)):
+            mfcc = wav2mfcc(wavfile, max_len=max_len)
+            mfcc_vectors.append(mfcc)
+        np.save('data\\' + label + '.npy', mfcc_vectors)
+        print('saved data : ', label)
+
+
+save_data_to_array()
